40868e4a4ea53731d7827ed425f71d695c7a8286
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2021-10-21 15:03:28 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K
+# We Nub
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
